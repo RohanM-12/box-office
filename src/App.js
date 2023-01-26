d57bf6503f833af57292c2571ec7b08bfc86209d
Rohan Mali <rohanmali.2002@gmail.com>
2023-01-26 20:19:06 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home Page
+      </Route>
+      <Route exact path="/starred">
+        This is starred Page
+      </Route>
+      <Route>Page not Found 404</Route>
+      {/* default page loaded when path is not matched */}
+    </Switch>
+  );
 }
 
 export default App;

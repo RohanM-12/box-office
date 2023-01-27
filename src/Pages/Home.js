54c94930f445c5e220eaea9d96c176f755f7982c
Rohan Mali <rohanmali.2002@gmail.com>
2023-01-27 14:46:24 +0530

    small changes to api fetch

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,26 +1,41 @@
 import { React, useState } from 'react';
-
+import { apiGet } from '../misc/config';
 import MainPageLayout from '../Components/MainPageLayout';
 
 function Home() {
   const [input, updateInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onInputChange = ev => {
     updateInput(ev.target.value);
   };
   const onSearchClick = () => {
-    //
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearchClick();
     }
   };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <MainPageLayout>
       <input
@@ -32,6 +47,7 @@ function Home() {
       <button type="button" onClick={onSearchClick}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 }

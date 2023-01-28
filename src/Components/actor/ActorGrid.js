66f2cd12c390d1cf5c443df7ecf48695df976c20
Rohan Mali <rohanmali.2002@gmail.com>
2023-01-28 13:48:53 +0530

    Created styled components and aplied them to actors and shows

--- src/Components/actor/ActorGrid.js
+++ src/Components/actor/ActorGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import IMAGE_NOT_FOUND from '../images/not-found.png';
 import ActorCard from './ActorCard';
+import { FlexGrid } from '../styled';
 
 function ActorGrid({ data }) {
   return (
-    <div>
+    <FlexGrid>
       {/* {console.log(data)} */}
       {data.map(({ person }) => (
         <ActorCard
@@ -17,7 +18,7 @@ function ActorGrid({ data }) {
           image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 }
 

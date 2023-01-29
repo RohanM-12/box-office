a74726f9011968b3b619675cbad8128f50850bc4
Rohan Mali <rohanmali.2002@gmail.com>
2023-01-29 17:51:58 +0530

    used reducer to manage states replaces all states in show

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,7 +9,6 @@ function Home() {
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
   const isShowsSearch = searchOption === 'shows';
-  // useEffect(() => {}, []);
 
   const onInputChange = ev => {
     updateInput(ev.target.value);

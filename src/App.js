8ea2fd88301d99bbae60f6e8d042abaaa84c43f6
Rohan Mali <rohanmali.2002@gmail.com>
2023-01-31 13:34:53 +0530

    added styles to navbar and title

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import { ThemeProvider } from 'styled-components';
 import Home from './Pages/Home';
 import Show from './Pages/Show';
 import Starred from './Pages/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/Starred">
-        <Starred />
-      </Route>
+        <Route exact path="/Starred">
+          <Starred />
+        </Route>
 
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-      <Route>Page not Found 404</Route>
+        <Route>Page not Found 404</Route>
 
-      {/* default page loaded when path is not matched */}
-    </Switch>
+        {/* default page loaded when path is not matched */}
+      </Switch>
+    </ThemeProvider>
   );
 }
 
